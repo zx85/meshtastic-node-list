465ba8ad3ed60a506638acbf04e0e1c8fa0f97a4
james <github@zx85.me>
2026-04-25 14:39:55 +0100

    Better python doings.

--- scripts/get_node_data.sh
+++ scripts/get_node_data.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-work_dir="/home/james/node_data"
+work_dir="/home/james/meshtastic-node-list/node_data"
 /home/james/.venv/bin/python -m meshtastic --serial /dev/ttyUSB0 --nodes > ${work_dir}/nodes_tmp.txt 2> ${work_dir}/error.log
 mv ${work_dir}/nodes_tmp.txt ${work_dir}/nodes.txt
